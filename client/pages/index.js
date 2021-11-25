2e550d17d6020b040a30619a3215bb43ef9c3109
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2021-11-25 16:52:41 +0700

    Added components folder

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,17 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
+import Head from "next/head"
+import Image from "next/image"
 
 export default function Home() {
   return (
-   <div>
-     <h1 >Hello World</h1>
-     <p>
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem eligendi odio voluptas, provident neque exercitationem quis perferendis consequuntur ipsam suscipit minima hic consectetur animi beatae iusto accusamus accusantium! Iste, obcaecati?
-     </p>
-     <p>
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod est facere recusandae. Magni eius reprehenderit repellendus laboriosam quibusdam aut expedita, iste recusandae similique? Reprehenderit necessitatibus quis, soluta explicabo officiis reiciendis!
-     </p>
-   </div>
+    <div className="flex flex-col justify-center items-center">
+      <h1>Hello World</h1>
+    </div>
   )
 }
